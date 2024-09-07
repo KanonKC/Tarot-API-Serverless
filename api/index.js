9987d79c74d425530eb9b1e788767286c3d481fe
KanonKC <kanonkce@gmail.com>
2024-09-07 18:43:51 +0700

    Limit card length

--- api/index.js
+++ api/index.js
@@ -1,14 +1,28 @@
 import Fastify from 'fastify'
 import { PrimaryCards } from './data/PrimaryCard'
+import { readFile } from 'fs'
 
 const app = Fastify({
     logger: true,
 })
 
 app.get('/', async (req, reply) => {
-    const randomCard = PrimaryCards[Math.floor(Math.random() * PrimaryCards.length)]
+    const randomCard = PrimaryCards[Math.floor(Math.random() * 2)]
     return reply.status(200).send(randomCard)
 })
+
+// app.get('/card-image/:id', async (req, reply) => {
+//     const imagePath = `api/imgs/${req.params.id}.png`
+//     readFile(imagePath, (err, data) => {
+//         if (err) {
+//             console.log("Image not found", err)
+//             return reply.status(404).send({ message: 'Image not found' })
+//         } else {
+//             console.log("Image found", data)
+//             return reply.status(201).send(data)
+//         }
+//     })
+// })
 
 export default async function handler(req, reply) {
     await app.ready()
