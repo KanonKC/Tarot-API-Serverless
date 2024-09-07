a364c2fd83caab699b1b84b45e22980f48d130bf
KanonKC <kanonkce@gmail.com>
2024-09-07 19:48:26 +0700

    Add secondary card

--- api/index.js
+++ api/index.js
@@ -1,24 +1,41 @@
 import Fastify from 'fastify'
 import { PrimaryCards } from './data/PrimaryCard'
 import { readFile } from 'fs'
+import { SecondaryCards } from './data/SecondaryCard'
 
 const app = Fastify({
     logger: true,
 })
 
-app.get('/', async (req, reply) => {
-    const randomCard = PrimaryCards[Math.floor(Math.random() * PrimaryCards.length)]
+function getPictureNumberAndIndex(id, diffIdStart, diffIdEnd, pictureNumberOffset) {
+    let picturePosition = id
 
-    let picturePosition = randomCard.id
-
-    if (randomCard.id >= 19 && randomCard.id <= 21) {
+    if (id >= diffIdStart && id <= diffIdEnd) {
         picturePosition += 1
     }
 
-    const pictureNumber = Math.floor(picturePosition / 8) + 1
+    const pictureNumber = Math.floor(picturePosition / 8) + 1 + pictureNumberOffset
     const pictureOneIndex = (picturePosition + 1) % 8
 
-    return reply.status(200).send({...randomCard, pictureNumber, pictureOneIndex})
+    return { pictureNumber, pictureOneIndex }
+}
+
+app.get('/', async (req, reply) => {
+
+    const randomCard = PrimaryCards[Math.floor(Math.random() * PrimaryCards.length)]
+    const randomSecondaryCard = SecondaryCards[Math.floor(Math.random() * SecondaryCards.length)]
+
+    const { pictureNumber, pictureOneIndex } = getPictureNumberAndIndex(randomCard.id, 19, 21, 0)
+
+    const secondaryBody = {
+        secondaryId: randomSecondaryCard.id,
+        secondaryName: randomSecondaryCard.name,
+        secondaryDescription: randomSecondaryCard.description,
+        secondaryPictureNumber: Math.floor((randomSecondaryCard.id - 22) / 8) + 4,
+        secondaryPictureOneIndex: (randomSecondaryCard.id - 21) % 8,
+    }
+
+    return reply.status(200).send({ ...randomCard, pictureNumber, pictureOneIndex, ...secondaryBody })
 })
 
 // app.get('/card-image/:id', async (req, reply) => {
